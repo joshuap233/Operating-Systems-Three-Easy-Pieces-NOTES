c5146eb5948176ea90d7fa741bf790edfa1b6b1b
pjshu <shushugo233@gmail.com>
2020-10-18 09:21:05 +0800

    第六章代码修改

--- 第六章-机制:受限制直接执行/课后问题代码/test-content-switch.c
+++ 第六章-机制:受限制直接执行/课后问题代码/test-content-switch.c
@@ -1,56 +1,84 @@
 //
 // Created by pjs on 2020/10/8.
 //
+
+#define _GNU_SOURCE //sched_setaffinity
+
 #include "sched.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 #include "sys/time.h"
 #include "unistd.h"
+#include <sched.h>
+
 
 int main() {
-  int fd1[2], fd2[2], fdt[2], pid, n;
+    int fd1[2], fd2[2], fdt[2], pid, n;
 
-  struct timeval tv_bg, tv_end;
-  struct sched_param param;
+    struct timeval tv_bg, tv_end;
+    const struct sched_param param = {sched_get_priority_min(SCHED_FIFO)};
+    cpu_set_t set;
+    // 将cpu 0 添加到集合
+    CPU_SET(0, &set);
 
-  printf("输入测试次数(次数太少会导致误差极大): ");
-  scanf("%d", &n);
+    printf("输入测试次数(次数太少会导致误差极大): ");
+    scanf("%d", &n);
 
-  if (pipe(fd1) < 0 || pipe(fd2) < 0 || pipe(fdt) < 0) {
-    perror("pipe");
-    exit(1);
-  };
+    if (pipe(fd1) < 0 || pipe(fd2) < 0 || pipe(fdt) < 0) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    };
 
-  if ((pid = fork()) < 0) {
-    printf("error");
-    exit(1);
-  } else if (pid == 0) {
-    // SCHED_FIFO指定先来先服务调度
-    sched_setscheduler(getpid(), SCHED_FIFO, &param);
+    if ((pid = fork()) < 0) {
+        printf("error");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // 需要root 权限
+        // SCHED_FIFO指定先来先服务调度,
+        if (sched_setscheduler(getpid(), SCHED_FIFO, &param) == -1) {
+            printf("程序运行需要root权限(sched_setscheduler系统调用)\n");
+            perror("sched_setscheduler");
+            exit(EXIT_FAILURE);
+        }
 
-    gettimeofday(&tv_bg, NULL);
-    for (int i = 0; i < n; i++) {
-      read(fd1[0], NULL, 1);
-      write(fd2[1], "h", 1);
+        // 线程绑定核心
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+
+
+        gettimeofday(&tv_bg, NULL);
+        for (int i = 0; i < n; i++) {
+            read(fd1[0], NULL, 1);
+            write(fd2[1], "h", 1);
+        }
+
+        write(fdt[1], &tv_bg, sizeof(tv_bg));
+        exit(EXIT_SUCCESS);
+
+    } else {
+        if (sched_setscheduler(getpid(), SCHED_FIFO, &param) == -1) {
+            perror("sched_setscheduler");
+            exit(EXIT_FAILURE);
+        }
+
+        if (sched_setaffinity(getpid(), sizeof(cpu_set_t), &set) == -1) {
+            perror("sched_setaffinity");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < n; i++) {
+            write(fd1[1], "h", 1);
+            read(fd2[0], NULL, 1);
+        }
+        gettimeofday(&tv_end, NULL);
+
+        read(fdt[0], &tv_bg, sizeof(tv_bg));
+
+        printf("上下文切换时间为: %f 微秒",
+               (float) (tv_end.tv_sec * 1000000 + tv_end.tv_usec - tv_bg.tv_sec * 1000000 - tv_bg.tv_usec) / 2 / n);
     }
-
-    write(fdt[1], &tv_bg.tv_usec, sizeof(tv_bg.tv_usec));
-    exit(0);
-
-  } else {
-    sched_setscheduler(getpid(), SCHED_FIFO, &param);
-
-    for (int i = 0; i < n; i++) {
-      write(fd1[1], "h", 1);
-      read(fd2[0], NULL, 1);
-    }
-    gettimeofday(&tv_end, NULL);
-
-    read(fdt[0], &tv_bg.tv_usec, sizeof(tv_bg.tv_usec));
-
-    printf("上下文切换时间为: %ld 微秒",
-           (tv_end.tv_usec - tv_bg.tv_usec) / (2 * n));
-  }
-  return 0;
+    return 0;
 }
--- 第六章-机制:受限制直接执行/课后问题代码/test-system-call.c
+++ 第六章-机制:受限制直接执行/课后问题代码/test-system-call.c
@@ -6,16 +6,17 @@
 #include <unistd.h>
 
 int main() {
-  int n;
-  printf("输入运行系统调用次数: ");
-  scanf("%d", &n);
+    int n;
+    printf("输入运行系统调用次数: ");
+    scanf("%d", &n);
 
-  struct timeval tv_begin, tv_end;
-  gettimeofday(&tv_begin, NULL);
-  for (int i = 0; i < n; i++) {
-    read(0, NULL, 0);
-  }
-  gettimeofday(&tv_end, NULL);
-  printf("执行时间为: %ld微秒", (tv_end.tv_usec - tv_begin.tv_usec) / n);
-  return 0;
+    struct timeval tv_begin, tv_end;
+    gettimeofday(&tv_begin, NULL);
+    for (int i = 0; i < n; i++) {
+        read(0, NULL, 0);
+    }
+    gettimeofday(&tv_end, NULL);
+    printf("执行时间为: %f微秒",
+           (float )(tv_end.tv_sec * 1000000 + tv_end.tv_usec - tv_begin.tv_sec * 1000000 - tv_begin.tv_usec) / n);
+    return 0;
 }
