f3a9b1c3806ddebb74400aaa0cbe158155d61142
pjshu <shushugo233@gmail.com>
2020-10-20 22:42:10 +0800

    第二十二章

--- 第二十二章-超越物理内存策略/run.py
+++ 第二十二章-超越物理内存策略/run.py
@@ -13,7 +13,7 @@ with open('./test.txt') as f:
         addr = re.findall(r" ([\dabcdef]*),", line)
         if len(addr) == 1:
             bit_length = int(addr[0], 16).bit_length()
-            page = int(addr[0], 16) >> (36 - (48 - bit_length))
+            page = int(addr[0], 16) >> (39 - (48 - bit_length))
             res.append(page)
     addr_list = ','.join([str(page) for page in res])
 
